e092783a43b3eff33955469e107af5c01398a378
zhuqiao <811879891@qq.com>
2020-05-12 00:26:44 +0800

    样式重叠删除图片
    样式重叠删除图片

--- component/uploadPhoto/uploadPhoto.js
+++ component/uploadPhoto/uploadPhoto.js
@@ -12,6 +12,8 @@ Component({
    */
   data: {
     imgPath:[
+      "/images/cm.jpg",
+      "/images/cm.jpg",
       "/images/cm.jpg"
     ]
   },
@@ -75,6 +77,28 @@ Component({
           });
         }
       })
-    }
-  },
+    },
+    deleteImg:function(e){
+      var that = this;
+      var imgs = that.data.imgPath;
+      var index = e.currentTarget.dataset.index;//获取当前长按图片下标
+      console.log(index)
+      wx.showModal({
+        title: '提示',
+        content: '确定要删除此图片吗？',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('点击确定了');
+            imgs.splice(index, 1);
+          } else if (res.cancel) {
+            console.log('点击取消了');
+            return false;
+          }
+          that.setData({
+            imgPath: imgs
+          })
+      }
+      })
+    },
+  }
 })
